9cfe0786cba34094a748cb0a96de72ea3b2d4393
Manaswini <manaswiniyadav8272@gmail.com>
2026-03-26 09:55:07 +0530

    Add files via upload

--- Unit -5/program.py
+++ Unit -5/program.py
@@ -1,19 +1,14 @@
-// 1. Create Nodes 
-CREATE (a:Person {name: 'Alice', age: 25}); 
-CREATE (b:Person {name: 'Bob', age: 27}); 
-CREATE (c:Person {name: 'Charlie', age: 30}); 
- 
-// 2. Create Relationships 
-CREATE (a)-[:FRIENDS_WITH]->(b); 
-CREATE (b)-[:FRIENDS_WITH]->(c); 
-CREATE (a)-[:COLLEAGUE_OF]->(c); 
- 
-// 3. Retrieve All Nodes 
-MATCH (n) RETURN n; 
- 
-// 4. Retrieve All Relationships 
-MATCH (a)-[r]->(b) RETURN a, r, b; 
- 
-// 5. Display Specific Relationship 
-MATCH (a:Person)-[:FRIENDS_WITH]->(b:Person) 
-RETURN a.name AS Person1, b.name AS Person2;
+// Neo4j Cypher Queries
+
+MATCH (p:Person)
+WHERE p.age > 26
+RETURN p.name, p.age;
+
+MATCH (:Person)-[r:FRIENDS_WITH]->(:Person)
+RETURN COUNT(r);
+
+MATCH (a:Person {name:'Alice'})-[:FRIENDS_WITH]->(friends)
+RETURN friends.name;
+
+MATCH (a)-[:FRIENDS_WITH]->(b)-[:FRIENDS_WITH]->(c)
+RETURN a.name, c.name;
